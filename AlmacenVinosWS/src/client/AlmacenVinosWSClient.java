8e6dd992fd19a384a22ddcb112ba44804bbaa69e
LUIS-MIGUEL ORTIZ ROZALEN <LuisMiguel.Ortiz1@alu.uclm.es>
2018-05-02 13:49:47 +0200

    v1.2

--- AlmacenVinosWS/src/client/AlmacenVinosWSClient.java
+++ AlmacenVinosWS/src/client/AlmacenVinosWSClient.java
@@ -9,7 +9,7 @@ public class AlmacenVinosWSClient {
 		proxy.setEndpoint("http://localhost:52606/AlmacenVinosWS.asmx");
 
 		try {
-			System.out.println(proxy.consulta());
+			System.out.println(proxy.cambiarStock("4", "1"));
 		} catch (Exception e) {
 			System.out.println("Excepción al conectarse " + e.getMessage());
 		}
